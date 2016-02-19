df21c33fe95e2a962069c13977fd4b89c961b212
Arthi <aarti156@gmail.com>
2016-02-18 18:14:13 -0800

    working on server.js file

---
+++ server/db/schema.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/cushion');
+
+var TechieSchema = new mongoose.Schema({
+  username: {type: String, unique: true},
+  password: String,
+  name: String,
+  email: String,
+  phoneNum: Number,
+  bio: String,
+  mobileApp: Boolean,
+  graphics: Boolean,
+  staticSite: Boolean,
+  interactiveSites: Boolean,
+  fullStackBeasts: Boolean
+});
+
+module.exports = mongoose.model('Techie', TechieSchema);
---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var bodyParse = require('body-parser');
+
+var app = express();
+
+app.get('/getTechies', )
+
+app.listen(8000);
+
+
+
+
+
+
+module.exports = app;
