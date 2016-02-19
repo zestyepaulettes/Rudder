1287ffa9bcdafd9a328c2421cc8c9e9594365fc0
Arthi <aarti156@gmail.com>
2016-02-18 18:57:08 -0800

    created the server file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,24 @@
 var express = require('express');
-var bodyParse = require('body-parser');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
 
 var app = express();
 
-app.get('/getTechies', )
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(_dirname + '/../../client'));
+
+app.get('/getTechies', function(req,res) {
+  db.find({req.body.x: true}, function(err, techie) {
+    if(err) {
+      res.send(404);
+    } else {
+      res.send(200, techie);
+    }
+  });
+});
 
 app.listen(8000);
-
-
-
-
-
 
 module.exports = app;
