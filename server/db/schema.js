8c02fc533581a483b4abe2c677965342eefd1943
Arthi <aarti156@gmail.com>
2016-02-18 18:16:15 -0800

    updated the schema

--- server/db/schema.js
+++ server/db/schema.js
@@ -3,17 +3,16 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cushion');
 
 var TechieSchema = new mongoose.Schema({
-            userName: {type: String, unique: true},
-            password: String,
-            Name: String,
-            email: String,
-            phoneNo: Number,
-            Bio: String,
-            mobileApp: Boolean,
-            graphics: Boolean,
-            staticSite: Boolean,
-            interactiveSites: Boolean,
-            fullStackBeasts: Boolean
+  useranme: {type: String, unique: true},
+  password: String,
+  email: String,
+  phonenum: Number,
+  bio: String,
+  mobileApp: Boolean,
+  graphics: Boolean,
+  staticSite: Boolean,
+  interactiveSites: Boolean,
+  fullStackBeasts: Boolean
 });
 
 module.exports = mongoose.model('Techie', TechieSchema);
