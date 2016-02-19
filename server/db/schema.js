e143019395761887b8561cebcf15c79e3d63100d
Arthi <aarti156@gmail.com>
2016-02-18 18:14:13 -0800

    working on server.js file

--- server/db/schema.js
+++ server/db/schema.js
@@ -3,17 +3,17 @@ var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/cushion');
 
 var TechieSchema = new mongoose.Schema({
-            userName: {type: String, unique: true},
-            password: String,
-            Name: String,
-            email: String,
-            phoneNo: Number,
-            Bio: String,
-            mobileApp: Boolean,
-            graphics: Boolean,
-            staticSite: Boolean,
-            interactiveSites: Boolean,
-            fullStackBeasts: Boolean
+  username: {type: String, unique: true},
+  password: String,
+  name: String,
+  email: String,
+  phoneNum: Number,
+  bio: String,
+  mobileApp: Boolean,
+  graphics: Boolean,
+  staticSite: Boolean,
+  interactiveSites: Boolean,
+  fullStackBeasts: Boolean
 });
 
 module.exports = mongoose.model('Techie', TechieSchema);
