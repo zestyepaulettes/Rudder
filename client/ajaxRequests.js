b18677ed61191c9caec3542caf2b699bb4e0c006
Yasunari Flores <carlosyasu91@gmail.com>
2016-02-22 10:58:41 -0800

    Started working on angellist api calls

--- client/ajaxRequests.js
+++ client/ajaxRequests.js
@@ -5,6 +5,15 @@ var getTechies = function(success, fail){
     error: fail
   });
 };
+
+var getAngelListData = function(success, fail){
+  $.get({
+    url: 'http://127.0.0.1:8000/api/techie',
+    success: success,
+    error: fail
+  });
+}
+
 window.getTechies = getTechies;
 
 var signUpTechie = function(newTechieObject, success, fail){
