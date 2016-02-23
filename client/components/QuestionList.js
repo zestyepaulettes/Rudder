81ce48eb886a4d2c0a8efa621b6686b40d996ad7
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-02-22 18:13:46 -0800

    Pass through method in props to Option

--- client/components/QuestionList.js
+++ client/components/QuestionList.js
@@ -5,7 +5,7 @@ var QuestionList = ({question, changeQuestion}) => {
     <h1>{question.question}</h1>
     <ul>
       {Object.keys(question.options).map((newQuestion, i)=> {
-        return (<Option text={question.options[newQuestion]} newQuestion={newQuestion} changeQuestion={changeQuestion}/>)
+        return (<Option text={question.options[newQuestion]} newQuestion={newQuestion} changeQuestion={changeQuestion} method = {question.method}/>)
       }
       )}
     </ul>
