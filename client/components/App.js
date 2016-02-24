a0f83e1f06ee1b31c0eecec8b5d1454cf3fb3d36
Yasunari Flores <carlosyasu91@gmail.com>
2016-02-24 15:26:19 -0800

    Added build grunt task to clean, concat, transpile, uglify and cssmin

--- client/components/App.js
+++ client/components/App.js
@@ -28,14 +28,6 @@ class App extends React.Component {
       currentQuestion: this.state.allQuestions.whyAreYouHere
     });
   }
-  // componentDidMount(){
-  //   var updateState = (results) => {
-  //     this.setState({
-  //       results: results
-  //     });
-  //   };
-  //   getTechies(updateState);
-  // }
 
   render() {
     return(
