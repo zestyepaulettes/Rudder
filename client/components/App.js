dbeeb749b81a0a3178751b8d76ef518de4b44fa1
Yasunari Flores <carlosyasu91@gmail.com>
2016-02-26 12:09:43 -0800

    Fixed home button

--- client/components/App.js
+++ client/components/App.js
@@ -1,4 +1,5 @@
 var Link = window.ReactRouter.Link;
+var hashHistory = window.ReactRouter.hashHistory;
 class App extends React.Component {
   constructor(props){
     super(props);
@@ -7,6 +8,13 @@ class App extends React.Component {
       results: [],
       currentQuestion: {}
     };
+  }
+
+  resetState(){
+    this.setState({
+      results: [],
+      currentQuestion: this.state.allQuestions.whyAreYouHere
+    });
   }
 
   changeQuestion(newQuestion){
@@ -35,7 +43,7 @@ class App extends React.Component {
     <div>
       <nav>
         <ul>
-          <li><Link to="/">Home</Link></li>
+          <li><Link to="/" onClick={this.resetState.bind(this)}>Home</Link></li>
           <li><Link to="/login">Signup/Login</Link></li>
           <li><Link to="/socialmedia">Social media</Link></li>
           <li><Link to="/">Contact us</Link></li>
