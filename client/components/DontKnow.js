d572d232e4c4df0f91931f88b82351b8604f87f7
Yasunari Flores <carlosyasu91@gmail.com>
2016-02-29 11:23:53 -0800

    Added DontKnow.js component

---
+++ client/components/DontKnow.js
@@ -0,0 +1,28 @@
+var DontKnow = (props) => (
+  <div>
+    <div id="facebook" className="container">
+      <h1>If you just want to be on the internet maybe you could use:</h1><br/>
+        <h2>Wordpress</h2> 
+        <p>WordPress is a powerful semantic publishing platform, and it comes with a great set of features designed to make your experience as a publisher
+         on the Internet as easy, pleasant, and appealing as possible. We are proud to offer you a freely distributed, standards-compliant, fast, light, and free content management
+         system, with sensible default settings and features, and an extremely customizable core.
+        <a href="https://wordpress.com/">Go to Wordpress</a></p>
+        <h2>Wix</h2>
+        <p>You could also use Wix to setup a free website for your company.
+        Wix is a free, user-friendly, website building platform. They help their users to create amazing, professional
+        looking sites, which can be updated and edited with the greatest of ease! No technical skills
+        are required and sites are 100% search engine friendly. They offer a wide range of site templates, or you can build your site from scratch.
+          <a href="http://www.wix.com/">Go to Wix</a>
+        </p>
+        <h2>Weebly</h2>
+        <p>Weebly is a global platform that lets people easily create a unique website, blog or online store. Millions around the world use Weebly to start their own business, sell online,
+         showcase their achievements and communicate with visitors in a thoughtful and meaningul way. Weebly gives everyone the freedom to build a high-quality site that works brilliantly across any device.
+          <a href="http://www.weebly.com/">Go to Weebly</a>
+        </p>
+    </div>
+  </div>
+);
+
+//we are not using this window object below, since we are using the react router.
+window.DontKnow = DontKnow;
+
