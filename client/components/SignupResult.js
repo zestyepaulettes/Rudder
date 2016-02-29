552c2d2389152a2959b98b658ccbf8605e2b2d69
Arthi <aarti156@gmail.com>
2016-02-29 12:28:37 -0800

    deleted the signup result component

--- client/components/SignupResult.js
+++
@@ -1,7 +0,0 @@
-var SignUpResult = (props) => (
-  <div>
-  <p> You have successfully created an account! You will be contacted soon if your skills match any of our customers needs!</p>
-  </div>
-);
-
-window.SignUpResult = SignUpResult;
