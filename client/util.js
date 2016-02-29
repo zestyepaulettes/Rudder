b2c290c2363c5740a698ac640d33d95fd846a273
Hamzah Chaudhary <hamzahchaudhary@gmail.com>
2016-02-29 09:29:25 -0800

    Add text to util.js file

--- client/util.js
+++ client/util.js
@@ -26,11 +26,11 @@ var ALL_QUESTIONS = {
   }
 }; 
 
-var tips = {sell: 'Click here if you would like to find people who can help you build a website to sell your products or services. Selling online can be an extremely lucrative additional revenue stream for many businesses, making you available to find by anyone accross the web. There are many different options when it comes to selling online including using what we call third-party \'widgets\' which make it easy to get up and running and are more than sufficient for most simple online transactions. For more complicated sales procedures (for specialized products or services), you may also consider having your own personal online shop built for you! Either way, click here to be connected with the right service providers who can definitely provide you with more information.', 
-upgrade:'Except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure consequences',
-advertise: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti',
-dontknow: 'am libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus',
-customizedWebsite: 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium',};
+var tips = {sell: 'Click here if you would like to find people who can help you build a website to sell your products or services. Selling online can be an extremely lucrative additional revenue stream for many businesses, making you available to find by anyone accross the web. There are many different options when it comes to selling online including using what we call third-party \'widgets\' which make it easy to get up and running and are more than sufficient for most simple online transactions. \n For more complicated sales procedures (for specialized products or services), you may also consider having your own personal online shop built for you! Either way, click here to be connected with the right service providers who can definitely provide you with more information.', 
+upgrade: 'Click here if you\'re looking to give your existing page a revamp or want to add more functionality. We can connect you with designers who can help you give your website a refresh.',
+advertise: 'If you\'re looking to try and build a bigger online presence, this is the button for you! This could mean you need a website to get more customers, you want to hear about how to make your website more attractive to drive more traffic to it (this is known as SEO- Search Engine Optimisation) or just to show off your products! Whatever you need to advertise your company, click here!'
+dontknow: 'If you have no idea what you need, this is the way forward for you! You can get setup with a really straightforward and simple online solution.',
+customizedWebsite: 'If you need something more heavy duty or you think you need something custom built, click here and we\'ll hook you up with one of our web whizzes (known in the business as full-stack engineers) who will be able to help you out with what you need.'};
 
 
 $(document).ready(function(){
