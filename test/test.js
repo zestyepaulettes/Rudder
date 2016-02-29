8ef1e682c0140312c0dd607a69282718101f5943
Yasunari Flores <carlosyasu91@gmail.com>
2016-02-29 14:46:11 -0800

    Added mock test

--- test/test.js
+++ test/test.js
@@ -2,5 +2,7 @@ var expect = chai.expect;
 var should = chai.should();
 
 describe('Compare Numbers' function(){
-  
+  it('1 should be equal to 1', function(){
+    expect(1).to.equal(1);
+  });
 });
