f9af645a3c9ffc0dca72319cb5ab741e9f6f7b10
Arthi <aarti156@gmail.com>
2016-02-29 16:48:53 -0800

    added documentation to questionList component

--- client/components/QuestionList.js
+++ client/components/QuestionList.js
@@ -4,6 +4,7 @@ var QuestionList = ({question, changeQuestion, loadTechies}) => {
   <div className="question-list">
     <h1>{question.question}</h1>
     <ul>
+    /* Object.keys is used to create an array of keys from question.options object and then mapped */
       {Object.keys(question.options).map((newQuestion, i)=> {
         return (<Option text={question.options[newQuestion]} newQuestion={newQuestion} changeQuestion={changeQuestion} loadTechies = {loadTechies} ajaxCall = {question.ajaxCall}/>)
       }
