ee0876d935c808c7208806cf595fb96e08b6ffe1
Arthi <aarti156@gmail.com>
2016-02-29 17:22:00 -0800

    fixed bug in question.js file

--- client/components/QuestionList.js
+++ client/components/QuestionList.js
@@ -1,11 +1,11 @@
+/* Object.keys is used to create an array of keys from question.options object 
+    The created array is mapped to separate Option componenets */
 var QuestionList = ({question, changeQuestion, loadTechies}) => {
   var arr = [];
   return (
   <div className="question-list">
     <h1>{question.question}</h1>
     <ul>
-    /* Object.keys is used to create an array of keys from question.options object 
-    The created array is mapped to separate Option componenets*/
       {Object.keys(question.options).map((newQuestion, i)=> {
         return (<Option text={question.options[newQuestion]} newQuestion={newQuestion} changeQuestion={changeQuestion} loadTechies = {loadTechies} ajaxCall = {question.ajaxCall}/>)
       }
